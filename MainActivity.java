8d36e6f2768b46508fa0d1f23ed6d6436f8bccd5
Kiran99 <techvil9@gmail.com>
2016-08-06 13:21:12 +0545

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,62 @@
+package gantabvya.com.gantabvya;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(R.string.tb_title);
+        getSupportActionBar().setSubtitle(R.string.tb_sub_title);
+        //getSupportActionBar().setIcon(R.drawable.ic_toolbar);
+
+        Intent searchIntent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(searchIntent.getAction())){
+
+            String query = searchIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_1:
+                Toast.makeText(MainActivity.this, "Option 1 clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_2:
+                Toast.makeText(MainActivity.this, "Option 2 clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
